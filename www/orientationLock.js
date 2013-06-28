19b3f11d7adf024b241b746839c5743db769b394
Shyamal Patel <shyamal.patel14@gmail.com>
2013-06-28 14:11:14 -0400

    added compatibility for phonegap/cordova 2.x.x (tested with 2.7.0)

--- www/orientationLock.js
+++ www/orientationLock.js
@@ -1,24 +1,31 @@
 function OrientationLock() {}
 
 OrientationLock.prototype.lock = function(orientation, win, fail) {
-    return PhoneGap.exec(
+    return cordova.exec(
             function (args) { if(win !== undefined) { win(args); } }, 
             function (args) { if(fail !== undefined) { fail(args); } }, 
             "OrientationLock", 
             "lock", 
-            [orientation]);	
+            [orientation]);    
 };
 
 OrientationLock.prototype.unlock = function(win, fail) {
-    return PhoneGap.exec(
-            function (args) { if(win !== undefined) { win(args); } }, 
+    return cordova.exec(
+            function (args) { if(win !== undefined) { win(args); } },
             function (args) { if(fail !== undefined) { fail(args); } },
             "OrientationLock", 
             "unlock", 
             []);	
 };
 
-PhoneGap.addConstructor(function() {
-    PhoneGap.addPlugin('OrientationLock', new OrientationLock());
-});
+//PhoneGap.addConstructor(function() {
+//    PhoneGap.addPlugin('OrientationLock', new OrientationLock());
+//});
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.OrientationLock) {
+    window.plugins.OrientationLock = new OrientationLock();
+}
 
