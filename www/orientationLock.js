528803a485856a7997f0440d29566e1eec045679
Christoph Neumann <christoph@neumannhaus.com>
2014-01-08 12:42:30 -0500

    Revamped the plugin to support Cordova 3.0+

--- www/orientationLock.js
+++ www/orientationLock.js
@@ -1,33 +1,18 @@
-cordova.define("orientationLock", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    
-    var OrientationLock = function() {};
+var OrientationLock = function() {}
 
-    OrientationLock.prototype.lock = function(orientation, win, fail) {
-        return exec(
-                function (args) { if(win !== undefined) { win(args); } }, 
-                function (args) { if(fail !== undefined) { fail(args); } }, 
-                "OrientationLock", 
-                "lock", 
-                [orientation]); 
-    };
-
-    OrientationLock.prototype.unlock = function(win, fail) {
-        return exec(
-                function (args) { if(win !== undefined) { win(args); } }, 
-                function (args) { if(fail !== undefined) { fail(args); } },
-                "OrientationLock", 
-                "unlock", 
-                []);    
-    };
-
-    var orientationLock = new OrientationLock();
-    module.exports = orientationLock;
-});
-
-if (!window.plugins) {
-    window.plugins = {};
+OrientationLock.prototype.lock = function(orientation, success, fail) {
+	return cordova.exec(success, fail, "OrientationLock", "lock", [orientation])
 }
-if (!window.plugins.orientationLock) {
-    window.plugins.orientationLock = cordova.require("orientationLock");
+
+OrientationLock.prototype.unlock = function(success, fail) {
+	return cordova.exec(success, fail, "OrientationLock", "unlock", [])
 }
+
+if ( ! window.plugins ) {
+  window.plugins = {}
+}
+if ( ! window.plugins.orientationLock ) {
+  window.plugins.orientationLock = new OrientationLock()
+}
+
+module.exports = OrientationLock
